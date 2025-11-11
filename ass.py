ddee3660900f5cfbc1aa123a2bb0590554731702
Skema_R <oduwos@gmail.com>
2025-11-12 00:24:06 +0100

    first python assignment at techrise

---
+++ ass.py
@@ -0,0 +1,106 @@
+# Class assignment
+
+# Number one done
+
+train = "Python is fun to learn"
+print(train)
+
+# Number 2
+
+# multiline message
+
+pythonMultiLine = """
+    Python is so powerful
+    it is easy to learn
+    And it is also loved by developers
+"""
+
+# caklling the multiline variabe
+
+print(pythonMultiLine)
+
+# section B
+
+text = "PYTHON"
+
+firstLetter = text[0]
+thirdLetter = text[2]
+lastLetter = text[5]
+
+# printing first, third and last letters
+
+print(firstLetter)
+print(thirdLetter)
+print(lastLetter)
+
+loop = "PYTHON"
+
+for letters in loop:
+    print(letters)
+
+length = "Banana"
+print(len(length))
+
+# Section C
+
+word = "Learning Python is cool"
+
+if "Python" in word:
+    print('Pyhon is here')
+else:
+    print('nope python is not here')
+
+
+if "java" in word:
+    print('java is here')
+else:
+    print('java is not here')
+
+#  handling bonus
+
+message = " Coding is fun"
+
+leeter = "n"
+appeared = 0
+
+for i in message:
+    if i == leeter:
+        appeared += 1
+
+print(f"the letter '{leeter}' occured {appeared} time(s)")
+
+# While i love python
+
+dWord = """
+    I love python because it is an interpreter base compiler language
+    It is rooted in high level language
+    and it is super to understand and also frowns at wrong indentation
+"""
+
+print(dWord.upper())
+
+
+
+# second book
+
+name = 'Ada'
+age = 18
+school = 'Bright Future Academy'
+
+print(f"My name is {name}, i am {age} years old, and i attend {school}")
+
+country = 'Nigeria'
+capital = 'Abuja'
+
+print(f"The capital of {country} is {capital}")
+
+first_name = 'john'
+middle_name = 'Paul'
+last_name= 'Okoro'
+
+print(f"Full name {first_name} {middle_name} {last_name}")
+
+food = 'Rice'
+drink = 'Juice'
+
+print(f" I love eating {food} and dinking {drink}")
