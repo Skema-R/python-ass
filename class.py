ee76526f0cdce8e5c097375a602febd3f51d79fe
Skema_R <oduwos@gmail.com>
2025-11-12 22:54:38 +0100

    Class work, uploaded to the same repo

---
+++ class.py
@@ -0,0 +1,50 @@
+# txt = "We are the so called \"vikings\" from the north"
+
+# print (txt)
+
+# tt = "map"
+# print(tt.capitalize())
+
+# ttt= "MAP"
+# print(ttt.casefold())
+
+# tttt= "how"
+# print(tttt.center())
+# print(tttt.count())
+
+
+
+
+
+
+
+
+# class work on 12th November
+
+motors = []
+motors.extend(["lexus", "camry", "nnoson", "Tesla", "Tayota", "Mercedes"])
+print(motors)
+
+motors = []
+motors.append(["lexus", "camry", "nnoson", "Tesla", "Tayota", "Mercedes"])
+print(motors)
+
+# motors.pop(2)   
+print(motors)
+# motors.remove('lexus')
+print(motors)
+
+
+motors.reverse()
+print (motors)
+
+motors.sort()
+print(motors)
+
+
+
+
+# SOmething to note for
+# 1. uncomment pop and remove
+# 2. I used a nested list for a reason
+# 3. Beware of hoe to run it for errors
